c31deb57ea5d38bcf070e856662086d1d82f62b5
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2014-05-15 01:02:44 +0700

    feat(*): initial commit
    This changes is dirty.
    
    Flow:
    1. Click `start` button to start the timer
    2. Wait for the alert after 25 minutes is passed
    3. Dismiss the alert and start over

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('mpokDoroTimerApp')
+  .controller('MainCtrl', function($scope, $interval, $window) {
+
+    // 25 minutes is equal to 1500 seconds
+    // 25 minutes is equal to 1500 * 1000 milliseconds
+    var timebox = 1500 * 1000;
+
+    var timer;
+
+    $scope.pomodoro = 0;
+
+    $scope.start = function() {
+      timer = $interval(function() {
+        var START = $scope.pomodoro !== 0;
+        // Every four pomodorO take a longer break
+        var FOURTH_POMODORO = $scope.pomodoro % 4 === 0;
+
+        if (START && FOURTH_POMODORO) {
+          $window.alert('Come on take a long break for 20 minutes!');
+        } else {
+          $window.alert('Come on take a short break for 5 minutes!');
+        }
+
+        // record the pomodoro
+        $scope.pomodoro += 1;
+
+        // Cancels a task associated with the `timer`
+        $interval.cancel(timer);
+      }, timebox);
+    };
+  });
